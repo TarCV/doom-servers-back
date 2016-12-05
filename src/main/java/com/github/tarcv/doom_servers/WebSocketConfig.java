8dd97b1a53b423eefd0f5561ba47c11ff2a5b030
TarCV <TarCV@users.noreply.github.com>
2016-12-06 00:37:06 +0300

    Resized buffer size, so `cvarlist` result can be sent

--- src/main/java/com/github/tarcv/doom_servers/WebSocketConfig.java
+++ src/main/java/com/github/tarcv/doom_servers/WebSocketConfig.java
@@ -1,10 +1,12 @@
 package com.github.tarcv.doom_servers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -16,5 +18,14 @@ public class WebSocketConfig implements WebSocketConfigurer
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler, "/gs-guide-websocket").setAllowedOrigins("*");
+    }
+
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(81920);
+        container.setMaxBinaryMessageBufferSize(81920);
+        return container;
     }
 }
